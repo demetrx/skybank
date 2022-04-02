bf2f0a5862aba8b78309564d18f417eac925f7cc
demetrx <mcbright7447@gmail.com>
2022-04-02 19:37:46 +0300

    maybe

--- app.py
+++ app.py
@@ -469,7 +469,3 @@ def payments():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("payments.html", cash=cash, sign=sign)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
